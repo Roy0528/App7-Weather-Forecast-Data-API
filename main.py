822d8113f56f0e132a2af93c43e3b94658abd7cf
Roy Wu <roy32011@gmail.com>
2023-12-12 14:53:30 +0800

    Day 33: Handling error (place does not exist)

--- main.py
+++ main.py
@@ -13,19 +13,22 @@ option = st.selectbox("Select data to view",
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    # Get the temperature/sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [data["main"]["temp"] - 273.15 for data in filtered_data]
-        dates = [data["dt_txt"] for data in filtered_data]
-        # Create a temperature plot
-        fig = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (°C)"})
-        st.plotly_chart(fig)
+        if option == "Temperature":
+            temperatures = [data["main"]["temp"] - 273.15 for data in filtered_data]
+            dates = [data["dt_txt"] for data in filtered_data]
+            # Create a temperature plot
+            fig = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (°C)"})
+            st.plotly_chart(fig)
 
-    if option == "Sky":
-        sky_condition = [data["weather"][0]["main"] for data in filtered_data]
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        image_path = [images[condition] for condition in sky_condition]
-        st.image(image_path, width=115)
+        if option == "Sky":
+            sky_condition = [data["weather"][0]["main"] for data in filtered_data]
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            image_path = [images[condition] for condition in sky_condition]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.warning("That place does not exist")
